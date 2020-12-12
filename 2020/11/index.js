1691e7d9dd0925fcc80302bed79d19f14a60ca9b
Marcel Svensson <svensson.marcel@gmail.com>
2020-12-12 02:52:21 +0100

    refactor direction-functions on 11

--- 2020/11/index.js
+++ 2020/11/index.js
@@ -9,93 +9,37 @@ const example = fs.readFileSync('./example.txt', 'utf-8').split('\n').map(row =>
 const rawInput = fs.readFileSync('./input.txt', 'utf-8', 'utf-8').split('\n').map(row => row.split(''));
 
 const getSurroundings = (data, i, j, extend = false) => {
-    const top = (i, j, extend) => {
-        let space = 'X'; 
-        do {
-            space = (i > 0) ? data[i-1][j] : 'oob';
-            i--;
-        } while (extend && space === '.');
-        return space;
-    }
     
-    const topright = (i, j, extend) => {
+    const direction = (i, j, callback, iDelta, jDelta, extend) => {
         let space = 'X'; 
         do {
-            space = (i > 0 && j < data[i].length-1) ? data[i-1][j+1] : 'oob';
-            i--;
-            j++;
+            space = callback(i, j);
+            i += iDelta;
+            j += jDelta;
         } while (extend && space === '.');
         return space;
     }
 
-    const right = (i, j, extend) => {
-        let space = 'X'; 
-        do {
-            space = (j < data[i].length-1) ? data[i][j+1] : 'oob';
-            j++;
-        } while (extend && space === '.');
-        return space;
-    }
-
-    const bottomright = (i, j, extend) => {
-        let space = 'X'; 
-        do {
-            space = (i < data.length-1 && j < data[i].length-1) ? data[i+1][j+1] : 'oob';
-            i++;
-            j++;
-        } while (extend && space === '.');
-        return space;
-    }
-
-    const bottom = (i, j, extend) => {
-        let space = 'X'; 
-        do {
-            space = (i < data.length-1) ? data[i+1][j] : 'oob';
-            i++;
-        } while (extend && space === '.');
-        return space;
-    }
-
-    const bottomleft = (i, j, extend) => {
-        let space = 'X'; 
-        do {
-            space = (i < data.length-1 && j > 0) ? data[i+1][j-1] : 'oob';
-            i++;
-            j--;
-        } while (extend && space === '.');
-        return space;
-    }
-
-    const left = (i, j, extend) => {
-        let space = 'X'; 
-        do {
-            space = (j > 0) ? data[i][j-1] : 'oob';
-            j--;
-        } while (extend && space === '.');
-        return space;
-    }
-
-    const topleft = (i, j, extend) => {
-        let space = 'X'; 
-        do {
-            space = (i > 0 && j > 0) ? data[i-1][j-1] : 'oob';
-            i--;
-            j--;
-        } while (extend && space === '.');
-        return space;
-    }
+    const top = (i, j) => (i > 0) ? data[i-1][j] : 'oob';
+    const topright = (i, j) => (i > 0 && j < data[i].length-1) ? data[i-1][j+1] : 'oob';
+    const right = (i, j) => (j < data[i].length-1) ? data[i][j+1] : 'oob';
+    const bottomright = (i, j) => (i < data.length-1 && j < data[i].length-1) ? data[i+1][j+1] : 'oob';
+    const bottom = (i, j) => (i < data.length-1) ? data[i+1][j] : 'oob';
+    const bottomleft = (i, j) => (i < data.length-1 && j > 0) ? data[i+1][j-1] : 'oob';
+    const left = (i, j) => (j > 0) ? data[i][j-1] : 'oob';
+    const topleft = (i, j) => (i > 0 && j > 0) ? data[i-1][j-1] : 'oob';
 
     // Stored clockwise from top to topleft;
     const surroundings = {
         adjacents: [ 
-            top(i, j, extend),
-            topright(i, j, extend),
-            right(i, j, extend),
-            bottomright(i, j, extend),
-            bottom(i, j, extend),
-            bottomleft(i, j, extend),
-            left(i, j, extend),
-            topleft(i, j, extend)
+            direction(i, j, top, -1, 0, extend),
+            direction(i, j, topright, -1, 1, extend),
+            direction(i, j, right, 0, 1, extend),
+            direction(i, j, bottomright, 1, 1, extend),
+            direction(i, j, bottom, 1, 0, extend),
+            direction(i, j, bottomleft, 1, -1, extend),
+            direction(i, j, left, 0, -1, extend),
+            direction(i, j, topleft, -1, -1, extend)
         ]
     };
 
