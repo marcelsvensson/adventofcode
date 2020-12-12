2161d945a50421a42f97d1e1f756fc21ad9c48b7
Marcel Svensson <svensson.marcel@gmail.com>
2020-12-12 02:57:59 +0100

    cleanup old attempts on 10

--- 2020/10/index.js
+++ 2020/10/index.js
@@ -5,42 +5,21 @@ console.log('* Advent of Code - 10 *');
 console.log('***********************\n');
 
 const example = fs.readFileSync('./example.txt', 'utf-8').split('\n').map(jolt => parseInt(jolt)).sort((a, b) => a - b);
-
 const example2 = fs.readFileSync('./example2.txt', 'utf-8').split('\n').map(jolt => parseInt(jolt)).sort((a, b) => a - b);
-
 const rawInput = fs.readFileSync('./input.txt', 'utf-8').split('\n').map(jolt => parseInt(jolt)).sort((a, b) => a - b);
-
-const runOptionsAdditive = (options, jolts) => {
-    let counter = 0;
-    for (const option of options) {
-        if (jolts[option] === jolts[jolts.length-1]) {
-            counter++;
-        } else {
-            let nextOption = option+1;
-            let nextOptions = [];
-            while(jolts[nextOption] - jolts[option] <= 3) {
-                nextOptions.push(nextOption);
-                nextOption++;
-            }
-            counter += runOptionsAdditive(nextOptions, jolts);
-        }
-    }
-    if (counter > 10000000) console.log(counter);
-    return counter;
-}
 
 let cached = {};
 
 const cache = (options, jolts) => {
     const cacheSum = 'c-' + options.join('-');
     if (!cached[cacheSum]) {
-        cached[cacheSum] = runOptionsAdditiveWithCache(options, jolts);
+        cached[cacheSum] = runOptions(options, jolts);
     }
 
     return cached[cacheSum];
 }
 
-const runOptionsAdditiveWithCache = (options, jolts) => {
+const runOptions = (options, jolts) => {
     let counter = 0;
     for (const option of options) {
         if (jolts[option] === jolts[jolts.length-1]) {
@@ -52,6 +31,8 @@ const runOptionsAdditiveWithCache = (options, jolts) => {
                 nextOptions.push(nextOption);
                 nextOption++;
             }
+
+            // earlier attempts did not use cache - instead inte called runOptions here directly
             counter += cache(nextOptions, jolts);
         }
     }
@@ -61,16 +42,10 @@ const runOptionsAdditiveWithCache = (options, jolts) => {
 
 const run = (jolts, condition = false, useCache = false) => {
     if (condition) {
-        if (!useCache) {
-            jolts.unshift(0);
-            const part2counterAdditive = runOptionsAdditive([0], jolts);
-            console.log(part2counterAdditive);
-        } else {
-            jolts.unshift(0);
-            cached = {};
-            const part2counterMultiplicative = runOptionsAdditiveWithCache([0], jolts);
-            console.log('🟢', part2counterMultiplicative);
-        }
+        jolts.unshift(0);
+        cached = {};
+        const part2counter = runOptions([0], jolts);
+        console.log('🟢', part2counter);
     } else {
         let previousJolt = 0;
         const joltDiff = ['zero', 0, 0, 0];
@@ -94,20 +69,14 @@ run(example);
 console.log('\npart 1 - INPUT')
 run(rawInput);
 
-console.log('\npart 2 - EXAMPLE - version 1 (too slow for input)')
+console.log('\npart 2 - EXAMPLE - version 2 (with cache)')
 run(example, true);
 
-console.log('\npart 2 - EXAMPLE 2 - version 1 (too slow for input)')
+console.log('\npart 2 - EXAMPLE 2 - version 2 (with cache)')
 run(example2, true);
 
-console.log('\npart 2 - EXAMPLE - version 2 (with cache)')
-run(example, true, true);
-
-console.log('\npart 2 - EXAMPLE 2 - version 2 (with cache)')
-run(example2, true, true);
-
 console.log('\npart 2 - INPUT - version 2 (with cache)')
-run(rawInput, true, true);
+run(rawInput, true);
 
 // 9256148959232 - appearantly too low =( - did not inject leading zero into my input
 // 16198260678656 - Yatta!
