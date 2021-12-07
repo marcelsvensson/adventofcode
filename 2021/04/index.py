83474c53d53eb9188a50deaf632886fbca166c0a
Marcel Svensson <svensson.marcel@gmail.com>
2021-12-07 01:10:39 +0100

    Added day 4 (part 1), 2021

---
+++ 2021/04/index.py
@@ -0,0 +1,48 @@
+
+print('***************************************');
+print('* Advent of Code - 04 - 2021 - Python *');
+print('***************************************\n');
+
+import numpy as np
+
+# My first attempt att numpy... took a bit too long to wrap my head around
+
+f=open("input.txt", "r")
+l=f.read().split('\n')
+
+steps = l[0].split(',')
+
+# Generate 2d-array from text
+boards = np.genfromtxt('input.txt', dtype='int8', autostrip=True, skip_header=1)
+# Calculate number of boards (this will be our third dimension)
+axis1, axis2 = boards.shape
+axis3D = int(axis1 / axis2)
+# Reshape into 3d-array
+boards = boards.reshape(axis3D,5,5)
+
+winner = False
+matchingMatrix = False
+numStep = []
+# Keep going while no winner is found
+while winner != True:
+    step = int(steps.pop(0))
+    numStep.append(step)
+    # Keep adding steps into a list, we then generate a bool-mask when we match values on each board   
+    isinVar = np.isin(boards, numStep)
+    # We then check this mask in both x- and y-axis to find complete rows
+    matchingMatrisAxis2 = np.any(np.all(isinVar, axis=2), axis=1)
+    matchingMatrisAxis1 = np.any(np.all(isinVar, axis=1), axis=1)
+    # Set the propper Axis
+    if True in matchingMatrisAxis2:
+        matchingMatrix = matchingMatrisAxis2
+    elif True in matchingMatrisAxis1:
+        matchingMatrix = matchingMatrisAxis1
+
+    if type(matchingMatrix) != bool:
+        # Use the mask to get the winning board and flatten to 1d
+        winningBoard = boards[matchingMatrix,:].flatten()
+        # (overkill) - reapply the mask but invert, then get the sum and multiply with the last step
+        print(np.sum(winningBoard[np.isin(winningBoard, numStep, invert=True)]) * step)
+        # We have a winner
+        winner = True
+
